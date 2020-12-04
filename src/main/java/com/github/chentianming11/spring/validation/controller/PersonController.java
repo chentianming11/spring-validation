de1e94d785be38480c6f90eaa4e4182e0024f5ef
chentianming <chentianming11@qq.com>
2020-12-04 11:27:51 +0800

    完善示例

--- src/main/java/com/github/chentianming11/spring/validation/controller/PersonController.java
+++ src/main/java/com/github/chentianming11/spring/validation/controller/PersonController.java
@@ -23,7 +23,6 @@ import java.util.List;
 public class PersonController {
 
 
-
     @ApiOperation("保存Person")
     @PostMapping("savePerson")
     public Result<Person> savePerson(@RequestBody @Valid Person person) {
@@ -31,19 +30,18 @@ public class PersonController {
     }
 
 
-
     @ApiOperation("查询person")
     @GetMapping("queryPerson")
-    public Result<List<Person>> queryPerson(@ApiParam(name = "用户id") @NotNull @Min(1000) @Max(10000000) Long id,
-                              @ApiParam(name = "姓名") @NotNull @Size(min = 2, max = 10) String name,
-                              @Max(200) Integer age,
-                              @Pattern(regexp = "^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\\.[a-zA-Z0-9_-]+)+$") String email) {
+    public Result<List<Person>> queryPerson(
+            @ApiParam("用户id") @Min(1000) @Max(10000000) Long id,
+            @ApiParam("姓名") @Size(min = 2, max = 10) String name,
+            @ApiParam("年龄") @Max(200) Integer age,
+            @ApiParam("邮箱") @Pattern(regexp = "^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\\.[a-zA-Z0-9_-]+)+$") String email) {
         List<Person> list = new ArrayList<>();
         list.add(new Person().setAge(10).setEmail("xxxxx").setId(1L).setName("哈哈"));
 
         return Result.ok(list);
     }
-
 
 
 }
